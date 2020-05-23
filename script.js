d65dc585de84d1bea40a18a12a52e2f3bf8b67eb
Rafael <rafael.cagliari22@gmail.com>
2020-05-22 21:18:10 -0300

    added mechanics for draw game

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 let player = 1;
-let board = [{'zero':''},{'one':''},{'two':''},{'three':''},{'four':''},{'five':''},
-{'six':''},{'seven':''},{'eight':''}];
-
 
 //manipulates the DOM in order to show the board array
+const boardFunctions= ()=>{
+
+const boardCreator = (board, number_of_plays)=>{
+    return { board, number_of_plays }
+}
+
 const gameBoardRender = () =>{
     let tic_tac_buttons = document.querySelectorAll('.tic-tac-button');
 for (let i=0; i < tic_tac_buttons.length; ++i){
-    if (Object.keys(board[i])==tic_tac_buttons[i].id){
-    tic_tac_buttons[i].innerText=Object.values(board[i])}};
-} ;
-
+    if (Object.keys(newBoard.board[i])==tic_tac_buttons[i].id){
+    tic_tac_buttons[i].innerText=Object.values(newBoard.board[i])}};
+} ; 
+return { gameBoardRender, boardCreator }
+}
 //makes an array using the board that returns true for every X ou O depending on the player
 //then compares the array to the possible winning combinations
 const checkWin = () =>{
         array = []
-        for (let i = 0; i<board.length ; ++i){
+        for (let i = 0; i<newBoard.board.length ; ++i){
             if(player==1){
-                if(Object.values(board[i])=='X'){
+                if(Object.values(newBoard.board[i])=='X'){
                     array.push(true)}
                 else{array.push(false)}}
             else{
-                if(Object.values(board[i])=='O'){
+                if(Object.values(newBoard.board[i])=='O'){
                     array.push(true)}
                 else{array.push(false)}
                 }}
@@ -29,9 +33,13 @@ const checkWin = () =>{
             (array[0] && array[3] && array[6]) || (array[1] && array[4] && array[7])||
             (array[2] && array[4] & array[6])||(array[2] && array[5] && array[8])||
             (array[3] && array[4] && array[5])|| (array[6] && array[7] && array[8])==true){
-                if(player==1){console.log(player1.name + ' wins'); player1.wins += 1; player1.winner='win'; gameFlow().restart()}
-                else{console.log(player2.name + ' wins'); player2.wins += 1;player2.winner='win'; gameFlow().restart()}};
-            ;
+                if(player==1){document.getElementById('match_result').innerText = player1.name + ' wins'; player1.wins += 1; player1.winner='win'; gameFlow().restart()}
+                else{document.getElementById('match_result').innerText = player2.name + ' wins'; player2.wins += 1;player2.winner='win'; gameFlow().restart()}};
+            if(newBoard.number_of_plays==9){
+                console.log('its a draw')
+                document.getElementById('match_result').innerText = "It's a draw!";
+                gameFlow().restart();
+            }
 } 
 
 const playerGenerator = (name, moves,winner, wins)=>{
@@ -63,28 +71,26 @@ const x_or_round=(elmnt)=> {
     if(start_switch.on_off=='off'){return}
     let element = elmnt.id;
     if(player==1){
-    for (let i=0; i<board.length ; ++i){
-        if(Object.keys(board[i])==element){
-            if (Object.values(board)[i][element] == 'X' || Object.values(board)[i][element] == 'O'){return}
-            Object.values(board)[i][element] = 'X'
+    for (let i=0; i<newBoard.board.length ; ++i){
+        if(Object.keys(newBoard.board[i])==element){
+            if (Object.values(newBoard.board)[i][element] == 'X' || Object.values(newBoard.board)[i][element] == 'O'){return}
+            Object.values(newBoard.board)[i][element] = 'X'
             player1.moves.push(element)
         }
     }}
     else if(player==2){
-        for (let i=0; i<board.length ; ++i){
-            if(Object.keys(board[i])==element){
-                if (Object.values(board)[i][element] == 'X' || Object.values(board)[i][element] == 'O'){return}
-                Object.values(board)[i][element] = 'O'
+        for (let i=0; i<newBoard.board.length ; ++i){
+            if(Object.keys(newBoard.board[i])==element){
+                if (Object.values(newBoard.board)[i][element] == 'X' || Object.values(newBoard.board)[i][element] == 'O'){return}
+                Object.values(newBoard.board)[i][element] = 'O'
                 player2.moves.push(element)
             }
         }
     }
-    gameBoardRender()
+    document.getElementById('match_result').innerText = ''
+    newBoard.number_of_plays += 1;
+    boardFunctions().gameBoardRender()
     checkWin()
-    /*if(player1.winner == 'win' || player2.winner == 'win'){
-        winnerDisplay();
-    }
-    else{*/
     playerTurnSwitcher()
     turnDisplay()
     scoreDisplay()}
@@ -92,11 +98,18 @@ const x_or_round=(elmnt)=> {
 
 const restart = () =>{
     start_switch.on_off='on';
-    board = [{'zero':''},{'one':''},{'two':''},{'three':''},{'four':''},{'five':''},
+    newBoard.board = [{'zero':''},{'one':''},{'two':''},{'three':''},{'four':''},{'five':''},
     {'six':''},{'seven':''},{'eight':''}];
     player1.moves = []; 
     player2.moves = [];
-    gameBoardRender()}
+    newBoard.number_of_plays = 0;
+    boardFunctions().gameBoardRender()}
+
+const newGame = () =>{
+    restart();
+    start_switch.on_off='off'
+    startGame();
+}
 
 const turnDisplay= () =>{
     if(player==1){
@@ -107,18 +120,7 @@ const turnDisplay= () =>{
 const scoreDisplay=()=>{
     document.getElementById('score').innerText=`${player1.name}: ${player1.wins} | ${player2.name}: ${player2.wins}`
 }
-/*const winnerDisplay=()=>{
-    if(player1.winner=='win'){
-        document.getElementById('turn').innerText=`${player1.name} wins! Press restart match to begin a new match.`;
-        player1.winner ==''
-    }
-    else if(player2.winner=='win'){
-        document.getElementById('turn').innerText=`${player2.name} wins! Press restart match to begin a new match.`;
-        player2.winner==''
-    }
-}*/
-return { setPlayers, playerTurnSwitcher, x_or_round, restart, turnDisplay};
-
+return { setPlayers, playerTurnSwitcher, x_or_round, restart, newGame, turnDisplay};
 }
 
 //this functions created a switch object which will then be altered by other functions
@@ -127,7 +129,7 @@ const startSwitch=(on_off)=>{
     return { on_off }
 }
 //start game creates 2 player objects with the names from the form on the html file 
-document.getElementById('start_game').addEventListener('click', function(){
+const startGame= ()=>{
     if(start_switch.on_off=='on'){return};
     start_switch.on_off='on';
     if(document.getElementById('player1_name').value== '' && (document.getElementById('player2_name').value== '')){
@@ -138,8 +140,10 @@ document.getElementById('start_game').addEventListener('click', function(){
         gameFlow().setPlayers();
         gameFlow().turnDisplay();
         document.getElementById('score').innerText=`${player1.name}: ${player1.wins} | ${player2.name}: ${player2.wins}`
-    };
-    
-});
-
+    };  
+};
 let start_switch = startSwitch('off');
+
+let newBoard = boardFunctions().boardCreator([{'zero':''},{'one':''},{'two':''},{'three':''},{'four':''},{'five':''},
+{'six':''},{'seven':''},{'eight':''}], 0)
+
